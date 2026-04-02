e6428e16dcfb131c72607556247fc458ec579608
Meetthefella <lornaanddave@hotmail.com>
2026-04-02 08:41:39 +0100

    harden api utility button actions

--- app.js
+++ app.js
@@ -9201,6 +9201,20 @@ async function testApiConnection(){
   }
 }
 
+function saveApiSettingsFromUi(){
+  saveState();
+  setStatus('apiStatus', '<span class="ok">API settings saved on this device.</span>');
+  return false;
+}
+
+async function testApiConnectionFromUi(){
+  await testApiConnection();
+  return false;
+}
+
+window.saveApiSettingsFromUi = saveApiSettingsFromUi;
+window.testApiConnectionFromUi = testApiConnectionFromUi;
+
 async function autoAnalyseWatchlist(options = {}){
   return runScannerWorkflow(options);
 }
